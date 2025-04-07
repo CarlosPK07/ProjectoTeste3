0f7425639112228b1f25773f96eeabed3143ef74
CarlosPK07 <carlospedrokiaku@gmail.com>
2025-04-07 21:34:10 +0100

    Forçando novo deploy

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/check-env', function () {
+    return response()->json([
+        'env' => env('APP_ENV'),
+        'debug' => env('APP_DEBUG'),
+        'url' => env('APP_URL'),
+    ]);
+});
