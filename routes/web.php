24214890591a16bdc42e180b8f48b0235ad68256
CarlosPK07 <carlospedrokiaku@gmail.com>
2025-04-07 21:42:23 +0100

    Adicionando rota /check-env para debug

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,20 @@ Route::get('/check-env', function () {
         'url' => env('APP_URL'),
     ]);
 });
+
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/check-env', function () {
+    return response()->json([
+        'env' => env('APP_ENV'),
+        'debug' => env('APP_DEBUG'),
+        'url' => env('APP_URL'),
+        'db_connection' => env('DB_CONNECTION'),
+        'db_host' => env('DB_HOST'),
+        'db_port' => env('DB_PORT'),
+        'db_database' => env('DB_DATABASE'),
+        'db_username' => env('DB_USERNAME'),
+        'db_password' => env('DB_PASSWORD'),
+    ]);
+});
